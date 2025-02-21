7305d607af24ef1eeacd4cff64e15587f72f23a4
NavodContrath <navod.contrath@gmail.com>
2025-02-21 14:07:39 +0100

    add array and loop for random fouls and points

--- main.js
+++ main.js
@@ -56,7 +56,44 @@ console.log(result);
 const solution = Math.min.apply(null, weightBikes)
 console.log(solution);
 
+/* 
+Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
+const soccerTeams = [{
+    name: 'Milan',
+    points: 0,
+    fouls: 0,
+}, {
+    name: 'Inter',
+    points: 0,
+    fouls: 0,
+}, {
+    name: 'Juve',
+    points: 0,
+    fouls: 0,
+}, {
+    name: 'Napoli',
+    points: 0,
+    fouls: 0,
+}, {
+    name: 'Roma',
+    points: 0,
+    fouls: 0,
+},
+]
+
+
+for (let i = 0; i < soccerTeams.length; i++) {
+    const thisTeam = soccerTeams[i];
+    thisTeam.points = Math.floor(Math.random() * 10)
+    thisTeam.fouls = Math.floor(Math.random() * 10)
+}
+console.log(soccerTeams);
 
 
 
