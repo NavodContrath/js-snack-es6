c95da9959c0c04a750469f9ca123320e3b6a42d6
NavodContrath <navod.contrath@gmail.com>
2025-02-21 13:49:54 +0100

    add first snack

--- main.js
+++ main.js
@@ -2,13 +2,6 @@
 Snack 1
 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.
-
-Snack2
-Creare un array di oggetti di squadre di calcio.
-Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
  */
 
 //Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
@@ -50,16 +43,20 @@ for (let i = 0; i < bikes.length; i++) {
     const thisBike = bikes[i];
     weightBikes.push(thisBike.weight)
 }
-console.log(weightBikes[0]);
+//solution with another for loop
 let result = weightBikes[0]
 for (let i = 0; i < weightBikes.length; i++) {
     const thisWeight = weightBikes[i];
-    console.log(thisWeight);
     if (thisWeight < result) {
         result = thisWeight
     }
 }
 console.log(result);
+//Solution with math min
+const solution = Math.min.apply(null, weightBikes)
+console.log(solution);
+
+
 
 
 
