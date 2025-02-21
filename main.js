339a23a23320fc5b77a92b698307713be2f95b6a
NavodContrath <navod.contrath@gmail.com>
2025-02-21 14:13:20 +0100

    add for loop task 2

--- main.js
+++ main.js
@@ -64,6 +64,8 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
+//Creare un array di oggetti di squadre di calcio.
+//Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 const soccerTeams = [{
     name: 'Milan',
     points: 0,
@@ -86,14 +88,20 @@ const soccerTeams = [{
     fouls: 0,
 },
 ]
-
-
+//creiamo un nuovo array 
+const nameAndFouls = []
+//Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 for (let i = 0; i < soccerTeams.length; i++) {
     const thisTeam = soccerTeams[i];
     thisTeam.points = Math.floor(Math.random() * 10)
     thisTeam.fouls = Math.floor(Math.random() * 10)
+    //push nel nuovo array
+    nameAndFouls.push(thisTeam.name, thisTeam.fouls)
 }
+//stampiamo tutto in console
 console.log(soccerTeams);
+console.log(nameAndFouls);
+
 
 
 
